6b3df83a1edf54fe3187c6a5ef6b43fbdf40076c
Daniel Aldén .NET <Daniel Aldn .NET@DESKTOP-0QL2OQM>
2023-10-21 17:57:58 +0200

    added simple console question switching

--- HPResultReader/HP.Console/ConsoleUI.cs
+++ HPResultReader/HP.Console/ConsoleUI.cs
@@ -11,12 +11,14 @@ namespace HP.Console
     {
         private IEnumerable<QuarterTest> _tests;
         private IEnumerable<Char> _possibleAnswers;
+
         public ConsoleUI(IEnumerable<QuarterTest> tests,
             IEnumerable<Char> possibleAnswers)
         {
             this._tests = tests;
             this._possibleAnswers = possibleAnswers;
         }
+
         public QuarterTest ChooseQuarterResult()
         {
             bool runningMenu = true;
@@ -28,50 +30,83 @@ namespace HP.Console
                 {
                     System.Console.WriteLine($"\nProvpass{test.TestId}");
                 }
+
                 var input = System.Console.ReadLine();
                 foreach (var test in _tests)
                 {
                     if (input.ToLower() == test.TestId.ToString()) return test;
                     if (input.ToLower() == "q") return new QuarterTest();
                 }
+
                 System.Console.WriteLine("Felaktig inmatning, avsluta med input Q");
             }
+
             return null;
         }
+
         public bool Run(QuarterTest test)
         {
             System.Console.WriteLine($"Provpass{test.TestId} startad, lycka till");
             var correctAnswers = 0;
             var startTime = DateTime.Now.Minute;
+
+            var answering = true;
             string input;
-            foreach (var answer in test.Answers)
+            bool changeQuestion;
+            for (int i = 0; i < test.Answers.Count(); i++)
             {
                 do
                 {
-                    System.Console.WriteLine($"Fråga {answer.AnswerId}:");
-                
+                    System.Console.WriteLine($"Fråga {test.Answers.ToList()[i].AnswerId}:");
+
                     input = System.Console.ReadLine().ToLower();
+                    changeQuestion = input.Contains(':');
+                    if (changeQuestion) answering = false;
+                    else if (!_possibleAnswers.Select(c => c.ToString())
+                                 .Contains(input))
+                    {
+                        System.Console.WriteLine("felaktig inmatning");
+                        answering = true;
+                    }
+                } while (answering);
 
-                    if (!_possibleAnswers.Select(c => c.ToString())
-                        .Contains(input)) System.Console.WriteLine("felaktig inmatning");
-                
-                } while (!_possibleAnswers.Select(c => c.ToString().ToLower())
-                .Contains(input.ToLower()));
-
-                if (input == answer.AnswerChar.ToString())
+                if (input.First() == ':')
+                {
+                    string questionIndex = input.Substring(1);
+                    if (QuestionIdValidator(test, questionIndex))
+                    {
+                        if (!Int32.TryParse(questionIndex, out i)) System.Console.WriteLine("Felaktig + inmatning");
+                        else
+                        {
+                            //TODO: add better implementation later
+                            System.Console.WriteLine($"\nByt till fråga {test.Answers.ToList()[i-1].AnswerId}\n");
+                            i = i - 2 ;
+                        }
+                    }
+                }
+                else if (input == test.Answers.ToList()[i].AnswerChar.ToString())
                 {
                     correctAnswers++;
                     System.Console.WriteLine($"\n{input.ToUpper()} är rätt!");
                 }
-                else System.Console.WriteLine($"\n{input.ToUpper()} är fel" +
-                    $"\nRätt svar är {answer.AnswerChar.ToString().ToUpper()}");
+                else
+                    System.Console.WriteLine($"\n{input.ToUpper()} är fel" +
+                                             $"\nRätt svar är {test.Answers.ToList()[i].AnswerChar.ToString().ToUpper()}");
             }
+
             var stopTime = DateTime.Now.Minute;
             System.Console.WriteLine($"\nResultat: {correctAnswers}/{test.Answers.Count()}" +
-                $"\nTid: Inte implementerat ännu." +
-                $"\nFortsät med samma eller ett annat Provpass? (y/n)");
+                                     $"\nTid: Inte implementerat ännu." +
+                                     $"\nFortsät med samma eller ett annat Provpass? (y/n)");
             input = System.Console.ReadLine();
             return input == "y" ? true : false;
+        }
+
+        private bool QuestionIdValidator(QuarterTest test, string questionId)
+        {
+            var foundAnswers = test.Answers.ToList().FirstOrDefault(a => a.AnswerId == questionId);
+            if (foundAnswers != null) return true;
+            return false;
         }
     }
 }
--- HPResultReader/HP.Console/Program.cs
+++ HPResultReader/HP.Console/Program.cs
@@ -1,21 +1,25 @@
 ﻿using HP.Console;
 using HP.Helpers;
+using System;
 
 var pdf = new PDFHandler();
 var resultHandler = new ResultHandler();
 
-string extractedPdf = pdf.ExtractResults("C:/Users/Daniel Aldén .NET/Desktop/HP program/Test-facit.pdf");
+Console.WriteLine("Skriv filens sökväg (filepath)");
+var filepath = Console.ReadLine();
+
+string extractedPdf = pdf.ExtractResults(filepath);
 var results = resultHandler.ConvertToQuarterTests(extractedPdf);
 
 
-var ui = new ConsoleUI(results, new Char[] {'a','b','c','d','e'});
+var ui = new ConsoleUI(results, new Char[] { 'a', 'b', 'c', 'd', 'e' });
 
 bool running = true;
 
 while (running)
-{   
+{
     var test = ui.ChooseQuarterResult();
-    if(test != null)
+    if (test != null)
     {
         running = ui.Run(test);
     }
